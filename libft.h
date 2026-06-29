1bd522b4e096673a34e82c5fe65573981c5c1ce6
Oishik Bhanja Choudhury <oishikchoudhuryit@gmail.com>
2026-06-29 13:55:03 +0200

    add ft_strchr.c and ft_strrchr.c

--- libft.h
+++ libft.h
@@ -18,4 +18,6 @@ size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t ft_strlcat(char *dst, const char *src, size_t size);
 int ft_toupper(int c);
 int ft_tolower(int c);
+char *ft_strchr(const char *s,int c);
+char *ft_strrchr(const char *s, int c);
 #endif
