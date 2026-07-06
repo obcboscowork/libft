716e4dbd3831fce5a664451f0d937c6c9cbb894c
Oishik Bhanja Choudhury <oishikchoudhuryit@gmail.com>
2026-07-06 22:17:20 +0200

    ft_strdup updated

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: obhanja <obhanja@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 19:34:08 by obhanja           #+#    #+#             */
-/*   Updated: 2026/07/06 20:04:44 by obhanja          ###   ########.fr       */
+/*   Updated: 2026/07/06 20:28:00 by obhanja          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 
 	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
 	if (len > s_len - start)
 		len = s_len - start;
 	str = malloc(len + 1);
